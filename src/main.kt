5860e225646ce89ef1de7c713a7b9b9c6175624d
Gabriel Giovanini <gabrielg.desouza@gmail.com>
2019-02-13 23:25:48 -0200

    Adds bootstrap for Immo Landwerth compiler tutorial

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,16 @@
 package rush
 
-fun main(args : Array<String>) {
-    println("Hello, world!")
+fun main(args: Array<String>) {
+    while (true) {
+        print("> ")
+        val line = readLine()
+        if(line.isNullOrEmpty())
+            return;
+
+        if(line == "1 + 2 + 3") {
+            println("?")
+        } else {
+            println("ERROR")
+        }
+    }
 }
